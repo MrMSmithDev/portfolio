bdb178e3088fc524f974853f28bb9337e634ed44
Michael <MichaelSmith90Dev@gmail.com>
2025-02-07 17:10:54 +0000

    Add page metadata

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,25 @@
+import About from '@components/about';
 import Hero from '@components/hero';
+import Projects from '@components/projects';
 import React from 'react';
+
+import Head from 'next/head';
 
 const Index: React.FC = () => {
   return (
-    <main className="bg-blue-bg" >
-      <Hero />
-    </main>
+    <>
+      <Head>
+        <title>Michael Smith</title>
+        <meta name="description" content="A web development portfolio" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <main className="bg-blue-bg">
+        <Hero />
+        <About />
+        <Projects />
+      </main>
+    </>
   );
 };
 
