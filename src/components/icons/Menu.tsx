df75885941c401b26675a109c9a4b798081a05ac
Michael <MichaelSmith90Dev@gmail.com>
2025-02-07 16:10:17 +0000

    Add icons, prop type and exports

---
+++ src/components/icons/Menu.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface IconProps {
+  className?: '';
+}
+
+const MenuSVG: React.FC<IconProps> = ({ className = '' }) => {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="none">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <path
+          d="M4 6H20M4 12H20M4 18H20"
+          stroke="#ffe066"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+export default MenuSVG;
---
+++ src/components/icons/index.ts
@@ -0,0 +1,21 @@
+import GitHubSVG from './Github';
+import JavascriptIcon from './Javascript';
+import MenuSVG from './Menu';
+import MongoDBIcon from './MongoDBIcon';
+import NextJSIcon from './NextJSIcon';
+import NodeJSIcon from './NodeJSIcon';
+import ReactIcon from './React';
+import SCSSIcon from './ScssIcon';
+import TypescriptIcon from './Typescript';
+
+export {
+  GitHubSVG,
+  JavascriptIcon,
+  MenuSVG,
+  MongoDBIcon,
+  NextJSIcon,
+  NodeJSIcon,
+  ReactIcon,
+  SCSSIcon,
+  TypescriptIcon,
+};
