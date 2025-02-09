03467b49b851cf9ec6e411038f96294e96eda02d
Michael <MichaelSmith90Dev@gmail.com>
2025-02-09 10:40:00 +0000

    Implement curves on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import About from '@components/about';
+import Head from 'next/head';
 import Hero from '@components/hero';
 import Projects from '@components/projects';
 import React from 'react';
-
-import Head from 'next/head';
+import Contact from '@components/contact';
+import { Curves } from '@components/utils';
 
 const Index: React.FC = () => {
   return (
@@ -17,7 +18,10 @@ const Index: React.FC = () => {
       <main className="bg-blue-bg">
         <Hero />
         <About />
+        <Curves from="gray-100" to="blue-bg" />
         <Projects />
+        <Curves from="blue-bg" to="gray-100" />
+        <Contact />
       </main>
     </>
   );
