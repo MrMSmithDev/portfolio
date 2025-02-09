74dd0ea636529290f16e86f89849cc2cb2bfcbe1
Michael <MichaelSmith90Dev@gmail.com>
2025-02-09 11:15:36 +0000

    Resolve issue for width of marquee on small width viewports

--- src/components/about/Marquee/Marquee.tsx
+++ src/components/about/Marquee/Marquee.tsx
@@ -12,7 +12,7 @@ import {
 
 const Marquee: React.FC = () => {
   return (
-    <div className="marquee relative flex overflow-x-hidden w-100 md:w-200 before:absolute before:content-[''] before:top-0 before:bottom-0 before:left-0 before:z-10 before:w-[20%] before:bg-linear-to-r before:from-gray-100 before:to-transparent after:absolute after:content-[''] after:top-0 after:bottom-0 after:right-0 after:w-[20%] after:z-10 after:bg-linear-to-l after:from-gray-100 after:to-transparent">
+    <div className="marquee relative flex overflow-x-hidden w-100 max-w-full md:w-200 before:absolute before:content-[''] before:top-0 before:bottom-0 before:left-0 before:z-10 before:w-[20%] before:bg-linear-to-r before:from-gray-100 before:to-transparent after:absolute after:content-[''] after:top-0 after:bottom-0 after:right-0 after:w-[20%] after:z-10 after:bg-linear-to-l after:from-gray-100 after:to-transparent">
       <div className="py-5 animate-marquee whitespace-nowrap">
         <JavascriptIcon className="inline-block mx-4 w-10 h-10 md:w-30 md:h-30" />
         <NextJSIcon className="inline-block mx-4 w-10 h-10 md:w-30 md:h-30" />
