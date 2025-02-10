ac1666fa2beaba9754dfb1184e6be7f286149ccc
Michael <MichaelSmith90Dev@gmail.com>
2025-02-10 17:09:08 +0000

    Add track element for video captioning

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,21 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import GitHubSVG from '@components/icons/Github';
+import React  from 'react';
 import Title from './Title';
 import Header from '@components/Header/Header';
 
 const Hero: React.FC = () => {
-  const [videoSrc, setVideoSrc] = useState('/assets/video/hero-bg.mp4');
-
-  useEffect(() => {
-    setVideoSrc(
-      'https://pub-e22900b9aca041d9893362a0fc9beb4b.r2.dev/hero-bg.mp4'
-    );
-  }, []);
-
-  // const videoUrl =
-  //   process.env.ENV === 'dev'
-  //     ? '/assets/video/hero-bg.mp4'
-  //     : 'https://pub-e22900b9aca041d9893362a0fc9beb4b.r2.dev/hero-bg.mp4';
   return (
     <section id="hero" className="min-h-screen">
       <video
@@ -25,7 +12,13 @@ const Hero: React.FC = () => {
         playsInline
         className="absolute w-full h-full object-cover z-0"
       >
-        <source src={videoSrc} type="video/mp4" />
+        <track
+          src="/assets/video/silent-captions.vtt"
+          kind="metadata"
+          srcLang="en"
+          label="English"
+        />
+        <source src='https://pub-e22900b9aca041d9893362a0fc9beb4b.r2.dev/hero-bg.mp4' type="video/mp4" />
       </video>
       <Header />
       <div className="absolute flex items-center justify-center w-full title-container md:md-title-container">
