6e69aa9bc6ea454d5b432b30181372d7393da971
Michael <MichaelSmith90Dev@gmail.com>
2025-02-11 12:50:57 +0000

    Add blurred header logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -54,9 +54,11 @@ const Header = () => {
   }, [dropMenuIsActive]);
 
   return (
-    <header className={`fixed top-0 left-0 right-0 grid grid-cols-[auto_1fr] md:grid-cols-[2fr_2fr] content-center h-10 md:h-20 p-2 sm:p-5 md:p-10 z-10 slide-from-top border-b-solid border-b-primary ${
-      blurredHeader ? "border-b-2 backdrop-blur-xl" : "border-0"
-    }`}>
+    <header
+      className={`fixed top-0 left-0 right-0 grid grid-cols-[auto_1fr] md:grid-cols-[2fr_2fr] content-center h-10 md:h-20 p-2 sm:p-5 md:p-10 z-10 slide-from-top border-b-solid border-b-primary ${
+        blurredHeader ? 'border-b-2 backdrop-blur-xl' : 'border-0'
+      }`}
+    >
       <a
         className="flex justify-center items-center border-primary border-3 border-solid h-[30px] w-[50px] md:h-[50px] cursor-pointer z-10 hover:animate-gelatine"
         href="#hero"
@@ -82,7 +84,6 @@ const Header = () => {
         <button
           onClick={() => handleScroll('#contact')}
           className="flex items-center justify-center text-center p-2 text-primary relative overflow-hidden transition-colors duration-200 ease-in-out h-8 md:h-10 w-full my-auto border-b-solid border-b-2 border-b-gray-100 cursor-pointer before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-primary before:transition-all before:duration-300 before:ease-in-out hover:text-blue-bg hover:before:w-full"
-
         >
           <span className="font-bold z-10">CONTACT</span>
         </button>
@@ -102,21 +103,18 @@ const Header = () => {
         <button
           onClick={() => handleScroll('#about')}
           className="flex items-center justify-center text-center p-2 text-primary relative overflow-hidden transition-colors duration-200 ease-in-out h-8 md:h-10 w-full my-auto border-b-solid border-b-2 border-b-gray-100 cursor-pointer before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-primary before:transition-all before:duration-300 before:ease-in-out hover:text-blue-bg hover:before:w-full"
-
         >
           <span className="font-bold z-10">ABOUT</span>
         </button>
         <button
           onClick={() => handleScroll('#projects')}
           className="flex items-center justify-center text-center p-2 text-primary relative overflow-hidden transition-colors duration-200 ease-in-out h-8 md:h-10 w-full my-auto border-b-solid border-b-2 border-b-gray-100 cursor-pointer before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-primary before:transition-all before:duration-300 before:ease-in-out hover:text-blue-bg hover:before:w-full"
-
         >
           <span className="font-bold z-10">PROJECTS</span>
         </button>
         <button
           onClick={() => handleScroll('#contact')}
           className="flex items-center justify-center text-center p-2 text-primary relative overflow-hidden transition-colors duration-200 ease-in-out h-8 md:h-10 w-full my-auto border-b-solid border-b-2 border-b-gray-100 cursor-pointer before:content-[''] before:absolute before:top-0 before:left-0 before:w-0 before:h-full before:bg-primary before:transition-all before:duration-300 before:ease-in-out hover:text-blue-bg hover:before:w-full"
-
         >
           <span className="font-bold z-10">CONTACT</span>
         </button>
